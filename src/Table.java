23f80731a8559aafb06f910b8937c290829cdbd0
Arda Önal <arda.onal@ug.bilkent.edu.tr>
2022-04-23 22:50:17 +0200

    login screen

---
+++ src/Table.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Table extends ArrayList<Record>
+{
+    private ArrayList<Record> records;
+
+    public Table()
+    {
+        records = new ArrayList<Record>();
+    }
+}
